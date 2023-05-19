17e587de690dc270b01aac1be2f2a7336c4f86d7
nookworth <nooksack@gmail.com>
2023-05-19 15:09:51 -0600

    First commit

--- lib/api_challenge.ex
+++ lib/api_challenge.ex
@@ -1,22 +1,10 @@
 defmodule ApiChallenge do
-  @moduledoc """
-  Documentation for `ApiChallenge`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ApiChallenge.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 
   def api_calls do
-    
+    # 5 processes that each make an api call at the same time
+  end
 
+  def test_url do
+    "https://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q=Salt_Lake_City&days=6&aqi=no&alerts=no"
+  end
 end
