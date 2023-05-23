0a1647921a93dd93237f8ac4afcfbb4e2b7e7a7e
Christopher Morrison <Chris@Tests-MBP.localdomain>
2023-05-23 14:52:06 -0600

    API call working from functions.

--- lib/api_challenge.ex
+++ lib/api_challenge.ex
@@ -1,10 +1,35 @@
 defmodule ApiChallenge do
+  use Agent
+  use HTTPoison.Base
+  @derive [Poison.Parser]
 
-  def api_calls do
-    # 5 processes that each make an api call at the same time
+
+  def setup do
+    {:ok, slc} = Agent.start(fn -> [] end)
+    {:ok, la} = Agent.start(fn -> [] end)
+    {:ok, boise} = Agent.start(fn -> [] end)
+    {slc, la, boise}
   end
 
-  def test_url do
-    "https://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q=Salt_Lake_City&days=6&aqi=no&alerts=no"
+  def url_constructor(location) do
+    "http://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q=" <>
+      location <> "&days=6&aqi=no&alerts=no"
+  end
+
+  slc_loc = "Salt_Lake_City"
+  la_loc = "Los_Angeles"
+  boise_loc = "43.6150,-116.2023"
+
+  def agent_func(url) do
+    # define functions to be used in Agent.update
+    response = HTTPoison.get(url)
+    response
+  end
+
+  def api_call(agent, loc) do
+    url = url_constructor(loc)
+    response = HTTPoison.get(url)
+    # json_response = Poison.Parser.parse!(response)
+    Agent.get_and_update(agent, fn list -> {list, list ++ response} end)
   end
 end
