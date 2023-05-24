49da919796b7298700cd7452f1d73fb5d56db3df
nookworth <nooksack@gmail.com>
2023-05-24 12:40:00 -0600

    API response stored in process state.

--- lib/api_challenge.ex
+++ lib/api_challenge.ex
@@ -1,8 +1,9 @@
 defmodule ApiChallenge do
   use Agent
-  use HTTPoison.Base
-  @derive [Poison.Parser]
+  use Tesla
 
+  # plug Tesla.Middleware.BaseUrl, "http://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q="
+  plug Tesla.Middleware.JSON
 
   def setup do
     {:ok, slc} = Agent.start(fn -> [] end)
@@ -12,24 +13,17 @@ defmodule ApiChallenge do
   end
 
   def url_constructor(location) do
-    "http://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q=" <>
-      location <> "&days=6&aqi=no&alerts=no"
-  end
-
-  slc_loc = "Salt_Lake_City"
-  la_loc = "Los_Angeles"
-  boise_loc = "43.6150,-116.2023"
-
-  def agent_func(url) do
-    # define functions to be used in Agent.update
-    response = HTTPoison.get(url)
-    response
+    "http://api.weatherapi.com/v1/forecast.json?key=7514621f9a034b1b9a1170939231805&q=" <> location <> "&days=6&aqi=no&alerts=no"
   end
 
   def api_call(agent, loc) do
     url = url_constructor(loc)
-    response = HTTPoison.get(url)
-    # json_response = Poison.Parser.parse!(response)
-    Agent.get_and_update(agent, fn list -> {list, list ++ response} end)
+    response = Tesla.get(url)
+    # Agent.get_and_update(agent, fn list -> {list, [response|list]} end)
+    IO.puts("Constructed URL: " <> url)
+
+    Agent.update(agent, fn list -> [response|list] end)
+
+    Agent.get(agent, fn state -> state end)
   end
 end
--- mix.exs
+++ mix.exs
@@ -23,8 +23,13 @@ defmodule ApiChallenge.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:httpoison, "~> 2.0"},
-      {:poison, "~> 5.0"}
+      {:tesla, "~> 1.4"},
+
+      # optional, but recommended adapter
+      {:hackney, "~> 1.17"},
+
+      # optional, required by JSON middleware
+      {:jason, "~> 1.4"}
     ]
   end
 end
