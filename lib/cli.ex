045723f670c8a37c35bd3defde677aa1f03873d8
nookworth <nooksack@gmail.com>
2023-07-03 11:16:55 -0600

    Multi-call function

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,18 +1,18 @@
 defmodule Cli do
   import Functions
 
-  loc_map = %{slc: "Salt_Lake_City", la: "Los_Angeles", boise: "43.6150,-116.2023"}
-
-  Functions.setup()
+  {slc, la, boise} = Functions.setup()
+  loc_map = %{"slc" => {slc, "Salt_Lake_City"}, "la" => {la, "Los_Angeles"}, "boise" => {boise, "43.6150,-116.2023"}}
 
   user_var = IO.gets("Choose slc, la, or boise: ")
   IO.puts("You selected: " <> user_var)
 
-  String.to_atom(user_var)
+  # String.to_atom(user_var)
 
-  loc_string = Map.get(loc_map, user_var)
-  # IO.puts("Your selection was mapped to: " <> loc_string)
-  IO.puts(loc_string)
+  target = Map.get(loc_map, user_var) |> Tuple.to_list() |> List.to_string()
+  # IO.puts(loc_string)
+  # IO.puts("Your selection was mapped to: ", target)
+  # IO.puts(loc_string)
 
-  # Functions.api_call(user_var, loc_string)
+  Functions.api_call(user_var, loc_string)
 end
--- lib/functions.ex
+++ lib/functions.ex
@@ -2,6 +2,8 @@ defmodule Functions do
   use Agent
   use Tesla
 
+  @locations %{slc: "Salt_Lake_City", la: "Los_Angeles", boise: "43.6150,-116.2023"}
+
   def setup do
     {:ok, slc} = Agent.start(fn -> [] end)
     {:ok, la} = Agent.start(fn -> [] end)
@@ -33,8 +35,18 @@ defmodule Functions do
     day_3_max_temp = Map.get(day_3_cond, "maxtemp_f")
     temp_avg = (day_1_max_temp + day_2_max_temp + day_3_max_temp)/3
 
-    IO.puts("The average max temperature over the next three days in " <> loc_string <> " is " <> Float.to_string(temp_avg) <> " degrees Fahrenheit.")
-
+    # IO.puts("The average max temperature over the next three days in " <> loc_string <> " is " <> Float.to_string(temp_avg) <> " degrees Fahrenheit.")
+    IO.puts(loc_string <> " Average Max Temp: " <> Float.to_string(temp_avg))
     Agent.update(agent, fn list -> [decoded|list] end)
+  end
+
+  # @spec multi_call(locations :: %{:slc :: String.to(), :la :: String.to(), :boise :: String.to()}) :: String.to()
+  def multi_call() do
+    {slc, la, boise} = setup()
+    # locations = @locations
+    # for pid <- pids, location <- locations, do: api_call(pid, location)
+    api_call(slc, "Salt_Lake_City")
+    api_call(la, "Los_Angeles")
+    api_call(boise, "43.6150,-116.2023")
   end
 end
